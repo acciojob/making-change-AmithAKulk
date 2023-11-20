3669a0b5baca9f2db7e13b7f4c61a03f3c495942
AmithAKulk <95955499+AmithAKulk@users.noreply.github.com>
2023-11-20 19:37:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
 const makeChange = (c) => {
-  // your name here
+//take the currencyvalue =c
+	let units={q:25,d:10,n:5,p:1}
+let output={};
+	for(let i in units){
+	  	let currentunit=units[i]; //1
+		//output[i]=c%currentunit;
+		output[i]= Math.floor(c / currentunit);
+		c=c-currentunit*output[i];
+	}
+	return output; 
 };
 
 // Do not the change the code below
